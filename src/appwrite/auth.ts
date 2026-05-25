8db47d356d4287167d070e6607bfce12f1a51063
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2026-05-25 15:48:08 +0530

    feat: implement AuthService for user authentication

---
+++ src/appwrite/auth.ts
@@ -0,0 +1,56 @@
+import { Account, Client, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // return (userAccount);
+                // ** login on register
+                return this.login({ email, password })
+            }
+            if (!userAccount) {
+                return null
+            }
+        } catch (err) {
+            return err
+        }
+    }
+
+    async login({ email, password }) {
+        // try {
+        const session = await this.account.createEmailPasswordSession(email, password)
+        return session
+        // } catch (err) {
+        //     throw err
+        // }
+    }
+    async getCurrentUser() {
+        try {
+            await this.account.get();
+        } catch (error) {
+            console.log(`Error on get current user ${error}`)
+        }
+        return null
+    }
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log(`Error on user LOGOUT ${error}`)
+
+        }
+    }
+}
+const authService = new AuthService();
+
+export default authService
