0bce074b359b78df0fbe809b056c3c4952684afb
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2026-05-27 16:55:59 +0530

    feat: implement authentication flow with Redux and add Header/Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
 
+import { useEffect, useState } from 'react'
 import './index.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth';
+import { login, logout } from './features/authSlice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-  <h1 className="text-3xl font-bold underline text">
-    TExt
-  </h1>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser().
+      then((userData) => {
+        if (userData) {
+          dispatch(login({ userData: userData }))
+        } else {
+          dispatch(logout())
+        }
+      }
+      )
+      .catch((error) => {
+        // dispatch(logout())
+        console.error(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  return !loading ? (
+    <>
+      <div className="min-h-dvh flex flex-wrap content-between bg-gray-400">
+        <div className="w-full">
+          <Header />
+          <main>
+            {/* <Outlet/>    */}
+            <h1>Hello World!</h1>
+          </main>
+          <Footer />
+        </div>
+      </div>
+
+    </>
   )
+
+
+    : (<>
+      {/* loading Component */}
+      <div>Loading</div>
+    </>);
 }
 
 export default App
